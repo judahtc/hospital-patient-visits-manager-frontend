e55617129cdaf4fc3afd1360e797ea2db3f345ff
judahtc <jaycea247@gmail.com>
2024-11-21 21:46:10 +0200

    feat: added formgroups to the login page

--- hospital-visits-manager-frontend/src/app/login/login.component.ts
+++ hospital-visits-manager-frontend/src/app/login/login.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  form!: FormGroup;
+  constructor(private fb: FormBuilder) {}
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      email: [''], // default value
+      password: [''], // default value
+    });
 
+    throw new Error('Method not implemented.');
+  }
+
+  login_func() {
+    console.log(this.form.value);
+  }
 }
