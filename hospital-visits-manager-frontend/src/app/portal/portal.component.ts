3c660835b93c63a4827262c7c278cf9b1e91eb44
judahtc <jaycea247@gmail.com>
2024-11-23 19:41:29 +0200

    feat: partially completed the dashboard page

--- hospital-visits-manager-frontend/src/app/portal/portal.component.ts
+++ hospital-visits-manager-frontend/src/app/portal/portal.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet, Route, Router } from '@angular/router';
+import { RouterOutlet, Route, Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-portal',
   standalone: true,
@@ -8,4 +8,60 @@ import { RouterOutlet, Route, Router } from '@angular/router';
   templateUrl: './portal.component.html',
   styleUrl: './portal.component.css',
 })
-export class PortalComponent {}
+export class PortalComponent implements OnInit {
+  dashboard = false;
+  patients = false;
+  visits = false;
+  users = false;
+
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit(): void {
+    this.route.queryParamMap.subscribe((params) => {
+      const tab = params.get('tab');
+      if (tab === 'dashboard') {
+        this.dashboard_func();
+      } else if (tab === 'patients') {
+        this.patients_func();
+      } else if (tab === 'visits') {
+        this.visits_func();
+      } else if (tab === 'users') {
+        this.users_func();
+      }
+    });
+  }
+
+  dashboard_func() {
+    this.dashboard = true;
+    this.patients = false;
+    this.visits = false;
+    this.users = false;
+
+    console.log(this.dashboard);
+    console.log(this.dashboard);
+    console.log(this.dashboard);
+    console.log(this.dashboard);
+    console.log(this.dashboard);
+    console.log(this.dashboard);
+  }
+
+  patients_func() {
+    this.dashboard = false;
+    this.patients = true;
+    this.visits = false;
+    this.users = false;
+  }
+
+  visits_func() {
+    this.dashboard = false;
+    this.patients = false;
+    this.visits = true;
+    this.users = false;
+  }
+
+  users_func() {
+    this.dashboard = false;
+    this.patients = false;
+    this.visits = false;
+    this.users = true;
+  }
+}
