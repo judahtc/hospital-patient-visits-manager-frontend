ef9967f83550d080c0ffe1e9bc0ae83a4d0e81d6
judahtc <jaycea247@gmail.com>
2024-11-23 13:36:14 +0200

    feat: added the portal skeletal design

--- hospital-visits-manager-frontend/src/app/app.routes.ts
+++ hospital-visits-manager-frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { PortalComponent } from './portal/portal.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'portal', component: PortalComponent },
   { path: '', component: LoginComponent },
 ];
