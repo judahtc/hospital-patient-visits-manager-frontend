c251a837eb36d616f6c71da491b721c7a1388a70
judahtc <jaycea247@gmail.com>
2024-12-01 22:11:05 +0200

    http backend

--- hospital-visits-manager-frontend/src/app/portal.guard.ts
+++ hospital-visits-manager-frontend/src/app/portal.guard.ts
@@ -1,5 +1,5 @@
 import { CanActivateFn } from '@angular/router';
-
+import { HttpBackend } from '@angular/common/http';
 export const portalGuard: CanActivateFn = (route, state) => {
   return true;
 };
