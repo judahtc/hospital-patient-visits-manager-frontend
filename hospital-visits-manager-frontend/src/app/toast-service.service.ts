184bd8c38c1c1f21463e7592f6cb06ec28de5160
judahtc <jaycea247@gmail.com>
2024-12-05 19:59:16 +0200

    feat: added into the toast service

--- hospital-visits-manager-frontend/src/app/toast-service.service.ts
+++ hospital-visits-manager-frontend/src/app/toast-service.service.ts
@@ -4,5 +4,26 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class ToastServiceService {
+  isVisible = false;
+  message = '';
+  toastType: 'success' | 'error' | 'warning' = 'success';
+  private timeout: any;
   constructor() {}
+
+  showToast(
+    message: string,
+    type: 'success' | 'error' | 'warning' = 'success'
+  ): void {
+    this.message = message;
+    this.toastType = type;
+    this.isVisible = true;
+
+    // Automatically hide the toast after 3 seconds
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => this.hideToast(), 3000);
+  }
+
+  hideToast(): void {
+    this.isVisible = false;
+  }
 }
