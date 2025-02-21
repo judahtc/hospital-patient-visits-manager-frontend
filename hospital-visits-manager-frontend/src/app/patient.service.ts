d431f25fe7e0e1c888bec7921121ef47e4122237
judahtc <jaycea247@gmail.com>
2025-02-21 18:56:25 +0200

    feat: created an http function to add a patient to the backend with url

--- hospital-visits-manager-frontend/src/app/patient.service.ts
+++ hospital-visits-manager-frontend/src/app/patient.service.ts
@@ -20,7 +20,7 @@ export class PatientService {
     return this.http.post('http://localhost:8002/visitors/', body);
   }
   add_patient(body: object) {
-    return this.http.post('http://localhost:8002/visitors/', body);
+    return this.http.post('http://localhost:8002/patients/', body);
   }
   deletePatient(id: any) {
     return this.http.get('http://localhost:8002/visitors/by-national-id/' + id);
