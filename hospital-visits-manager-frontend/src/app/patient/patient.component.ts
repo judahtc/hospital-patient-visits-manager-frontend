e8275e9a45a8d2c865997f5718d170830e29855a
judahtc <jaycea247@gmail.com>
2025-02-22 10:27:44 +0200

    feat: read email from path in patient

--- hospital-visits-manager-frontend/src/app/patient/patient.component.ts
+++ hospital-visits-manager-frontend/src/app/patient/patient.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-patient',
   standalone: true,
   imports: [],
   templateUrl: './patient.component.html',
-  styleUrl: './patient.component.css'
+  styleUrl: './patient.component.css',
 })
-export class PatientComponent {
-
+export class PatientComponent implements OnInit {
+  email: string = '';
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit(): void {
+    this.email = this.route.snapshot.params['id'];
+  }
 }
