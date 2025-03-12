a8a03d049915a1f09d3868c1c2f912150a14bf55
judahtc <jaycea247@gmail.com>
2025-03-12 20:23:23 +0200

    feat: added individual user

--- hospital-visits-manager-frontend/src/app/patient/patient.component.ts
+++ hospital-visits-manager-frontend/src/app/patient/patient.component.ts
@@ -12,6 +12,10 @@ import { PatientService } from '../patient.service';
 export class PatientComponent implements OnInit {
   email: string = '';
   patient: any;
+  deleteButton = false;
+  show = true;
+  type: string = '';
+  message: string = '';
 
   constructor(
     private route: ActivatedRoute,
@@ -28,5 +32,35 @@ export class PatientComponent implements OnInit {
         this.patient = result;
       },
     });
+  }
+
+  delete_user() {
+    this.deleteButton = false;
+    this.show = false;
+    this.type = '';
+    this.message = '';
+    let user_ids = JSON.parse(localStorage.getItem('selectedIds') || '[]'); // Parse the IDs from localStorage
+
+    if (Array.isArray(user_ids) && user_ids.length > 0) {
+      user_ids.forEach((id) => {
+        this.patientService.deletePatient(id).subscribe({
+          next: (res) => {
+            console.log(`Deleted user with ID`);
+            this.message = 'successfully deleted  patient with id:' + id;
+            this.type = 'success';
+            this.show = true;
+            localStorage.setItem('selectedIds', '');
+          },
+          error: (err) => {
+            this.message = 'failed to delete patient with id:' + id;
+            this.type = 'failed';
+            this.show = true;
+            console.error(`Failed to delete user with ID`);
+          },
+        });
+      });
+    } else {
+      console.warn('No user IDs found to delete.');
+    }
   }
 }
